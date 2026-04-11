54b04befb75b3004bcc1b247f243f1afc8b23b7f
hjriva <157857573+hjriva@users.noreply.github.com>
2026-04-11 08:27:57 -0300

    centralizacoes

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,11 @@ function dirPag(pag, guia) {
     .then(html => {
         consolecontent.innerHTML = html
     })
-
     guia.style.backgroundColor = 'black'
     window.document.getElementById('homespan').style.backgroundColor = '#2B2B2B'
 }
-
 let consolecontent = window.document.getElementById('consolecontent')
-
 let cttlink = window.document.getElementById('cttlink')
 let hablink = window.document.getElementById('hablink')
-
 cttlink.addEventListener('click', () => dirPag('paginas/contato.html', cttlink))
 hablink.addEventListener('click', () => dirPag('paginas/skills.html', hablink))
-
