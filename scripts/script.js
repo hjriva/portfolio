71b3cba298623df1d50799037fd407380ed73223
Helena <helenajungriva@gmail.com>
2026-07-11 11:37:35 -0300

    ajustes e novo projeto

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,17 @@ function dirPag(pag, guia) {
     .then(html => {
         consolecontent.innerHTML = html
     })
-    guia.style.backgroundColor = 'black'
-    window.document.getElementById('homespan').style.backgroundColor = '#2B2B2B'
+
+    document.querySelectorAll('nav a').forEach(aba => {
+        aba.id == guia ? aba.style.backgroundColor = 'black' : aba.style.backgroundColor = '#2B2B2B'
+    })
+
 }
+
 //Selecionando elementos de html e evento de click
 let consolecontent = window.document.getElementById('consolecontent')
 let cttlink = window.document.getElementById('cttlink')
 let hablink = window.document.getElementById('hablink')
-cttlink.addEventListener('click', (e) => {e.preventDefault(); dirPag('paginas/contato.html', cttlink)})
-hablink.addEventListener('click', (e) => {e.preventDefault(); dirPag('paginas/skills.html', hablink)})
+
+cttlink.addEventListener('click', (e) => {e.preventDefault(); dirPag('paginas/contato.html', 'cttlink')})
+hablink.addEventListener('click', (e) => {e.preventDefault(); dirPag('paginas/skills.html', 'hablink')})
